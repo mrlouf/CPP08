07fdef6cfbf1cdcd469f684bc7c1aeebbc18d1a5
nponchon <nponchon@student.42.fr>
2025-03-18 17:39:23 +0100

    starting ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 17:35:36 by nponchon          #+#    #+#             */
+/*   Updated: 2025/03/18 17:36:52 by nponchon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main()
+{
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::stack<int> s(mstack);
+	}
+
+		return 0;
+}
